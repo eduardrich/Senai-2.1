1d3705bc06d1f01bbe6292a047de345ebac96d27
eduardrich <eduard.sprung@gmail.com>
2021-06-20 15:07:59 -0300

    Alterado explicação

--- sexta.py
+++ sexta.py
@@ -1,10 +1,14 @@
 Horas = []
 
+#Pede para o usuario quantas horas deseja converter 
 for c in range(0,1):
     Horas.append ( int(input("Digite aqui o valor para converter para segundos: ")) )  
 
+#Transforma a lista em valor "Int"
 HorasInt = sum(Horas)
 
+#Converte a hora para segundo
 Segundos = HorasInt * 3600
 
+#Imprime os segundos
 print(Segundos)
